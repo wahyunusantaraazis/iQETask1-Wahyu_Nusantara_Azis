4e361c53192249e82b34762c1c02b3ac861d1ac0
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-11-05 23:53:48 +0700

    ubah value variabel

--- src/PART1/Problem1.java
+++ src/PART1/Problem1.java
@@ -5,8 +5,8 @@ public class Problem1 {
         float alas, tinggi; //ini untuk deklarasi variabel
         double luas; //ini untuk deklarasi variabel
         //input
-        alas = 20; //inisialisasi nilai variabel alas
-        tinggi = 25; //inisialisasi nilai varibale tinggi
+        alas = 50; //inisialisasi nilai variabel alas
+        tinggi = 15; //inisialisasi nilai varibale tinggi
         //kode
         luas = 0.5 * alas * tinggi;
         int intValue = (int) luas; //untuk proses konversi dari tipe data double ke integer
